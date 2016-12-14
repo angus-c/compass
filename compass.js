acce25b68e7525fcad2f8b82172409c89cc5c8c1
angus croll <anguscroll@gmail.com>
2016-12-13 21:28:11 -0800

    Update compass.js

--- compass.js
+++ compass.js
@@ -4,6 +4,7 @@ if (window.DeviceOrientationEvent) {
     document.querySelector('#compassHeading').innerText = Math.round(event.compassHeading || 0) + "'";   
     document.querySelector('#webkitCompassHeading').innerText = Math.round(event.webkitCompassHeading || 0) + "'";       
     document.querySelector('#compassAccuracy').innerText = Math.round(event.compassAccuracy || 0) + "'";           
+    document.querySelector('#webkitCompassAccuracy').innerText = Math.round(event.webkitCompassAccuracy || 0) + "'";               
   }, false);
 } else {
   alert('deviceorientation not supported in ' + navigator.userAgent)
