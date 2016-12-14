25f4cae1c97b74c4f1bf9664c31e6d5f66f5a95c
angus croll <anguscroll@gmail.com>
2016-12-13 21:39:14 -0800

    Update compass.js

--- compass.js
+++ compass.js
@@ -1,7 +1,7 @@
 if (window.DeviceOrientationEvent) {
   window.addEventListener('deviceorientation', function(event) {
-    document.querySelector('#alpha').innerText = Math.round(event.alpha) + "'";
-    document.querySelector('#compassHeading').innerText = Math.round(event.compassHeading || 0) + "'";   
+    document.querySelector('#alpha').innerText = Math.round(event.alpha) + "\u00B0";
+    document.querySelector('#compassHeading').innerText = Math.round(event.compassHeading || 0) + "\u00B0";   
     document.querySelector('#webkitCompassHeading').innerText = Math.round(event.webkitCompassHeading || 0) + "\u00B0";       
     document.querySelector('#compassAccuracy').innerText = Math.round(event.compassAccuracy || 0);           
     document.querySelector('#webkitCompassAccuracy').innerText = Math.round(event.webkitCompassAccuracy || 0);               
