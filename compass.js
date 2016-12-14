12a2352bcf968010598865bbc761762c96242ab9
angus croll <anguscroll@gmail.com>
2016-12-13 17:52:35 -0800

    Update compass.js

--- compass.js
+++ compass.js
@@ -6,3 +6,11 @@ if (window.DeviceOrientationEvent) {
 } else {
   alert('deviceorientation not supported in ' + navigator.userAgent)
 }
+
+if (!('oncompassneedscalibration' in window)) {
+  // oh nos
+} else {
+  window.addEventListener('compassneedscalibration', function(event) {
+    alert('Compass needs calibrating! Wave your device in a figure-eight motion');
+  });
+}  
